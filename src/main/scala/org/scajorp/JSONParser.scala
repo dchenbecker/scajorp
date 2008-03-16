29886a14d92b576ac7d33c4fa46c433c5772ce8f
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-03-16 11:49:47 +0000

    JsonClass match cases refactored.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@15 24bb8ead-1b48-0410-9278-47088e7565d6

--- src/main/scala/org/scajorp/JSONParser.scala
+++ src/main/scala/org/scajorp/JSONParser.scala
@@ -7,8 +7,8 @@ import scala.collection.Map
 class JSONParser {
 
   def resolve(jsonObject: Map[String, Any]): Option[Object] = {
-    jsonObject("jsonClass") match {
-        case className: String => createClassInstance(className, jsonObject.filterKeys(_ != "jsonClass"))
+    jsonObject.get("jsonClass") match {
+        case Some(className: String) => createClassInstance(className, jsonObject.filterKeys(_ != "jsonClass"))
         case _ => None
     }
 
@@ -17,7 +17,7 @@ class JSONParser {
   def createClassInstance(className: String, jsonFields: Map[String, Any]): Option[Object] = {
     try {
         val cls = Class.forName(className);
-        val instance = cls.newInstance().asInstanceOf[Object];        
+        val instance = cls.newInstance().asInstanceOf[Object];
 
         val setterFields : Map[String, Method] = createPublicSetterFields(cls);
 
@@ -28,7 +28,7 @@ class JSONParser {
                     case None => format("No such method name: {0}", jsonFieldName)
                   }
             }
-        Some(instance)    
+        Some(instance)
     }
         // should there simply be a check for Throwable
         // or is there gonna be e.g. a log message
@@ -37,7 +37,7 @@ class JSONParser {
         case genericError: Throwable => println(genericError); None;
     }
 
-    
+
 
   }
 
@@ -52,21 +52,21 @@ class JSONParser {
     **/
   private def createPublicSetterFields(cls: Class[_ <: Any]): Map[String, Method] = {
         import scala.collection.immutable.Map
-  
+
         val methods = cls.getMethods()
 
         methods.foldLeft(Map.empty[String, Method])({(resultMap, method) =>
             val name = method.getName
-            
+
             // Scala style setter check
             if (name.lastIndexOf("_$eq") == name.length - 4) {
               resultMap + Pair(name.substring(0,name.lastIndexOf("_$eq")),method)
             }
- 
+
             // Java style setter check
             else if (name.startsWith("set")) {
               resultMap + Pair(name.substring(3).toLowerCase,method)
-            } 
+            }
 
             // failure, just pass through
             else {
