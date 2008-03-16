1c90fc2263f0d9a7c5db162feb563f29e732baf3
dchenbecker <dchenbecker@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-03-16 15:03:04 +0000

    Reworked the resolve and create class instances to be a little cleaner. Class instance should also handle nested objects and arrays, although I need to write up more sophisticated test cases for that.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@17 24bb8ead-1b48-0410-9278-47088e7565d6

--- src/main/scala/org/scajorp/JSONParser.scala
+++ src/main/scala/org/scajorp/JSONParser.scala
@@ -6,12 +6,9 @@ import scala.collection.Map
 
 class JSONParser {
 
-  def resolve(jsonObject: Map[String, Any]): Option[Object] = {
-    jsonObject.get("jsonClass") match {
+  def resolve(jsonObject: Map[String, Any]): Option[Object] = jsonObject.get("jsonClass") match {
         case Some(className: String) => createClassInstance(className, jsonObject.filterKeys(_ != "jsonClass"))
-        case _ => None
-    }
-
+        case None => None
   }
 
   def createClassInstance(className: String, jsonFields: Map[String, Any]): Option[Object] = {
@@ -21,19 +18,28 @@ class JSONParser {
 
         val setterFields : Map[String, Method] = createPublicSetterFields(cls);
 
-        for( (jsonFieldName, value) <- jsonFields)
-            {
-                setterFields.get(jsonFieldName) match {
-                    case Some(method) => method.invoke(instance, Array(value.asInstanceOf[Object]))
-                    case None => format("No such method name: {0}", jsonFieldName)
-                  }
+        jsonFields.keys.foreach(fieldName => setterFields.get(fieldName) match {
+          case Some(method) => {
+            val arg : Object = jsonFields(fieldName) match {
+              // Handle nested objects. What to do if it doesn't resolve?
+              case obj : Map[String,Any] => resolve(obj).get
+              // Handle nested arrays
+              case array : List[Any] => array.toArray
+              // Handle simple values
+              case value : Object => value
             }
+
+            method.invoke(instance, Array(arg))
+          }
+          case None => format("No such method name: {0}", fieldName)
+        })
+
         Some(instance)
     }
         // should there simply be a check for Throwable
         // or is there gonna be e.g. a log message
-        // specific to one exception/error (InstantiationException, SecurityException etc..)?
     catch {
+        case cnf : ClassNotFoundException => throw cnf
         case genericError: Throwable => println(genericError); None;
     }
 
