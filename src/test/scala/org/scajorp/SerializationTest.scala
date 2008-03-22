3af56ccb38c67ae3fe610192c8e7b47f2306da38
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-03-22 16:40:58 +0000

    Added basic SerializationTest outline.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@22 24bb8ead-1b48-0410-9278-47088e7565d6

---
+++ src/test/scala/org/scajorp/SerializationTest.scala
@@ -0,0 +1,37 @@
+package org.scajorp
+
+
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+import org.junit.Assert._
+
+class SerializationTest {
+
+
+  /*  @Test
+    def simpleValueObject() {
+
+    }
+
+    @Test
+    def nestedSimpleValueObjects() {
+
+    }
+
+    @Test
+    def nestedComplexObjects() {
+    
+    }
+
+    @Test
+    def excludingProperties_annotations() {
+
+    }
+    
+    @Test
+    def excludingProperties_syntax() {
+
+    }*/
+
+}
