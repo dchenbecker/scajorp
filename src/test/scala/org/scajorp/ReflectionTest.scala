213185fc794247b53ba886f4d57fdfb1c9d1f2fb
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-03-22 19:11:13 +0000

    Added: Basic ReflectionTest outline.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@27 24bb8ead-1b48-0410-9278-47088e7565d6

--- src/test/scala/org/scajorp/ReflectionTest.scala
+++ src/test/scala/org/scajorp/ReflectionTest.scala
@@ -1,5 +1,26 @@
 package org.scajorp
 
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+import org.junit.Assert._
+
 class ReflectionTest {
-  // todo
+
+    @Test
+    def getReturnType() {
+        // TODO
+    }
+
+
+    @Test
+    def getParameterTypes() {
+        // TODO
+    }
+
+
+    @Test
+    def getModifiers() {
+        // TODO
+    }
 }
