2a481ed427c5bf0ab49470dfb3921d0033de3555
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-04-12 15:47:41 +0000

    Added: More tests to see how reflection in scala/java works.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@30 24bb8ead-1b48-0410-9278-47088e7565d6

--- src/test/scala/org/scajorp/ReflectionTest.scala
+++ src/test/scala/org/scajorp/ReflectionTest.scala
@@ -8,31 +8,66 @@ import org.junit.Assert._
 class ReflectionTest {
 
     val javaUser = new JavaUserDummy
-    val cls = classOf[JavaUserDummy]
+    val userDummy = new UserDummy
+    val javaClass = classOf[JavaUserDummy]
+    val scalaClass = classOf[UserDummy]
 
+
+   @Test
+   def getJavaClassName() {   
+        assertEquals("org.scajorp.JavaUserDummy", javaClass.getName())
+   }
+
+
+   @Test
+   def getJavaMethod() {
+        val method = javaClass.getMethod("setName", Array(classOf[String]));
+         assertNotNull("Method not found", method);
+         assertEquals("setName", method.getName());
+   }
+
+
+   // @Test(expected= classOf[NoSuchMethodException]) does not seem to work
+   @Test
+   def getNonExistingJavaMethod() {
+        try {
+            val method = javaClass.getMethod("setNonExisting", Array(classOf[String]));
+            fail("Method was found but shouldn't have been");
+        }
+        catch { case expected: NoSuchMethodException => {}}
+   }
+
+
+   @Test
+   def getJavaVoidReturnType() {
+       val method = javaClass.getMethod("setName", Array(classOf[String]));
+       val returnType = method.getReturnType();
+       assertEquals("void", returnType.getName())
+   }
+
+
+
+   //@Test
+   //def getAllScalaMethods() {
+   //      val methods = scalaClass.getMethods()
+   //      methods.foreach(method => println(method))
+   // }
 
 
 
    @Test
-   def getClassName() {   
-        assertEquals("org.scajorp.JavaUserDummy", cls.getName())
+   def getScalaMethod() {
+         val method = scalaClass.getMethod("age_$eq", Array(classOf[Int]));
+         assertNotNull("Method not found", method);
+         assertEquals("age_$eq", method.getName());
     }
 
-//    @Test
-//    def getJavaVoidReturnType() {
-//        val method = cls.  get
-//
-//    }
+   @Test
+   def getScalaMethodReturnType() {
+       val method = scalaClass.getMethod("age_$eq", Array(classOf[Int]));
+       val returnType = method.getReturnType();
+       assertEquals("void", returnType.getName())
+   }
 
 
-//    @Test
-//    def getParameterTypes() {
-//        // TODO
-//    }
-//
-//
-//    @Test
-//    def getModifiers() {
-//        // TODO
-//    }
 }
