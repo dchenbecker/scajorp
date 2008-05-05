ddca478af1b747b5ef538428f26860b222deea47
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-05-05 22:15:37 +0000

    [Modified]: JSONSerializer refactorings.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@40 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-api/src/main/scala/org/scajorp/JSONSerializer.scala
+++ scajorp-api/src/main/scala/org/scajorp/JSONSerializer.scala
@@ -8,21 +8,27 @@ class JSONSerializer {
     
     def serialize(obj :AnyRef):String = { 
       
-        var methods = obj.getClass.getMethods
-                          .toList
+        val getters = obj.getClass
+                           .getMethods.toList
+                            .filter(method => isGetter(method))
+                              .sort((s,t) => s.getName.charAt(0) <                                
+                                t.getName.charAt(0));
+         toJson(obj, getters);                                
         
-        methods = methods.filter(method => method.getDeclaringClass != classOf[AnyRef]
-                                 && !method.getName.equals("$tag")
-                                 && !method.getName.endsWith("_$eq"));
-        methods = methods.sort((s,t) => s.getName.charAt(0).toLowerCase <                                
-                               t.getName.charAt(0).toLowerCase);
-        val builder = new StringBuilder;
+    }
     
+    private def toJson(obj: AnyRef, methods: List[Method]): String = {
+        val builder = new StringBuilder;    
         builder.append("{")    
         methods.foreach(method =>  builder.append(buildJSONPair(obj, method)).append(","));       
-        builder.deleteCharAt(builder.length -1);
-        builder.append("}");    
-        builder.toString;
+        builder.deleteCharAt(builder.length -1).append("}").toString;        
+    }
+    
+
+    private def isGetter(method: Method): Boolean = {
+        method.getDeclaringClass != classOf[AnyRef] &&
+        !method.getName.equals("$tag") &&
+        !method.getName.endsWith("_$eq")                     
     }
 
 
