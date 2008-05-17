a403d0dfa6e30f9c8f61f17c249bf8cbc337b0d0
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-05-17 20:26:35 +0000

    [Modified]: Refactorings. Deleted self-explaining comments.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@62 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-api/src/main/scala/org/scajorp/json/JSONSerializable.scala
+++ scajorp-api/src/main/scala/org/scajorp/json/JSONSerializable.scala
@@ -1,44 +1,72 @@
 package org.scajorp.json 
 
+
+/**
+ * This trait is only supposed to be mixed-in/extended by JSONObject/JSONArray.
+ * Can transform both entities to a valid JSON-String.
+ */
 trait JSONSerializable {
- 
-    var opening_literal :String = _
-    var closing_literal :String= _
     
     private val builder = new StringBuilder
+ 
+    private var opening_literal :String = _  // { or [
     
-    /**
-     * Classes implementing JSONSerializable (JSONArray, JSONObject)
-     * can be serialized to a valid JSON String by invoking this method.
-     */
+    private var closing_literal :String = _ // } or [
+    
+            
     override def toString() = {  
         resetBuilder()
         serialize()        
     }
-               
-    /**
-     * Appends a JSON key-value pair to this trait's StringBuilder
-     * followed by a comma.
-     */
-    protected def appendObjectPair(key: String, value: Any) {
-        appendValue(key);
+      
+    private def serialize():String = {
+        setWrappingLiterals()
+        appendOpening()       
+        processBody()
+        appendClosing()
+        builder.toString()
+    }
+      
+
+    private def processBody(): Unit = {
+        this match {
+            case obj: JSONObject => processObject(obj)
+            case array: JSONArray => processArray(array)
+            case _ => println(">>> ERR: This isn't a JSONObject or JSONArray. Cannot process body.")
+        }
+    }
+    
+    private def processObject(jsonObject: JSONObject) {     
+        jsonObject.foreach(pair => appendObjectPair(pair._1, pair._2))        
+    }
+    
+    private def processArray(jsonArray: JSONArray) {       
+        jsonArray.foreach(value => appendArrayValue(value))                    
+    }
+  
+    /* ----- string building helpers ------ */
+    
+    private def append(obj: Any) = {
+        obj match {
+            case (s: String) => builder.append("\"").append(s).append("\"")
+            case (jsonObj : JSONObject) => builder.append(jsonObj.toString())
+            case (jsonArray : JSONArray) => builder.append(jsonArray.toString())
+            case _ => builder.append(obj)
+        }
+    }
+    
+    private def appendObjectPair(key: String, value: Any) {
+        append(key);
         appendSeparator()
-        appendValue(value)            
+        append(value)            
         appendComma()
     }
     
-    /**
-     * Appends a JSON array value to builder this trait's StringBuilder
-     * followed by a comma.
-     */
-    protected def appendArrayValue(value: Any) {
-        appendValue(value);
+    private def appendArrayValue(value: Any) {
+        append(value);
         appendComma()
     }
 
-
-    /* ----- Helper methods ----- */
-    
     private def appendComma() {
         builder.append(",")
     }
@@ -58,40 +86,17 @@ trait JSONSerializable {
     private def deleteLastComma() = {
         builder.deleteCharAt(builder.length -1)        
     }
-    
-    private def appendValue(obj: Any) = {
-        obj match {
-            case (s: String) => builder.append("\"").append(s).append("\"")
-            case (jsonObj : JSONObject) => builder.append(jsonObj.toString())
-            case (jsonArray : JSONArray) => builder.append(jsonArray.toString())
-            case _ => builder.append(obj)
-        }
-    }
-    
-    /**
-     * Clears this object's StringBuilder.  
-     * TODO see if needed
-     */
+        
     private def resetBuilder() {        
         if (builder.length() > 1) builder.delete(0, builder.length() - 1)        
-    }
-      
-    /**
-    * TODO
-    */
-    private def serialize():String = {
-        setWrappingLiterals()
-        appendOpening()       
-        process()
-        appendClosing()
-        builder.toString()
     }
       
     private def setWrappingLiterals() {
         this match {
             case obj: JSONObject => setObjectLiterals()
-            case array: JSONArray => setArrayLiterals()            
-       }
+            case array: JSONArray => setArrayLiterals()   
+            case _ => println(">>> ERR: This isn't a JSONObject or JSONArray. Cannot determine literals.")                
+        }
     }
            
     private def setObjectLiterals(){
@@ -105,21 +110,6 @@ trait JSONSerializable {
     }
         
 
-    private def process(): Unit = {
-        this match {
-            case obj: JSONObject => processObject(obj)
-            case array: JSONArray => processArray(array)
-            case _ => println("Oops, processing error")
-       }
-    }
-    
-
-    private def processObject(jsonObject: JSONObject) {     
-        jsonObject.foreach(pair => appendObjectPair(pair._1, pair._2))        
-    }
-    
-    private def processArray(jsonArray: JSONArray) {       
-        jsonArray.foreach(value => appendArrayValue(value))                    
-    }
+   
                
 }
