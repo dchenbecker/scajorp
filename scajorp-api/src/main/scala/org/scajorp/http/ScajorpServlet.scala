2b778f586e62843512a5cd51f558077a0bf647c7
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-05-24 17:24:51 +0000

    Bug problem? Help request
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@85 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-api/src/main/scala/org/scajorp/http/ScajorpServlet.scala
+++ scajorp-api/src/main/scala/org/scajorp/http/ScajorpServlet.scala
@@ -22,47 +22,32 @@ class ScajorpServlet extends HttpServlet{
       
       val jsonRequest = parseRequest(req)
       
-//      val app = new ScajorpApplication()
-//      app.register("calculator", classOf[Calculator])
-//      
-//      val result = app.execute(jsonRequest)
+      //testing, will be removed later
+      val app = new ScajorpApplication()
+      app.register("calculator", classOf[Calculator])
       
-      val response = "{\"jsonrpc\":\"2.0\",\"result\":"+19+",\"id\":1}"
+      val result = app.execute(jsonRequest)
+      
+      val response = "{\"jsonrpc\":\"2.0\",\"result\":"+result+",\"id\":1}"
       
       resp.getWriter().write(response)
       resp.getWriter().flush()
   }
   
   def parseRequest(req: HttpServletRequest): JSONRequest = {
-      val versionKey = "jsonrpc"
-      val methodKey = "method"
-      val paramsKey = "params"
-      val idKey = "id"
-      
+
       val jsonParser = new JSONParser()      
       val parsedResult = jsonParser.parseAll(jsonParser.obj, req.getReader())      
       
       //WTF!!!
       val result: scala.collection.Map[String, Any] = parsedResult.get 
       println("result: " + result)
-       println("result.contains(\"method\"): " + result("method"))
-      val version: String = result.get(versionKey) match {
-          case Some(s: String) => s
-          case None => error("Version is not a String")
-      }
-      val method: String = result.get(methodKey) match {
-          case Some(s: String) => s
-          case None => error("Method is not a String")
-      }
-      val params: Collection[Any] = result(paramsKey) match {
-          case Some(col: Collection[Any]) => col
-          case None => error("Params is not a Collection")
-      }
-      val id: Int = result(idKey) match {
-          case Some(i: Int) => i
-          case None => error("Id is not an int")
-      }
-      new JSONRequest(version, method, params, id)
+      result.keys.foreach(key => println("key " + key))
+      println("result.contains(\"method\"): " + result.get("method"))
+      println("result.contains(\"id\"): " + result.get("id"))
+      
+      //new JSONRequest(version, method, params, id)
+      new JSONRequest("2.0", "calculator.sum", List(10,9), 1)
   }
       
   
