26db694a7e5b2b6c7f7d76f7e049e7472dfd54b1
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-06-02 08:33:10 +0000

    Refactored JS.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@97 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-examples/src/main/webapp/client/scajorp.js
+++ scajorp-examples/src/main/webapp/client/scajorp.js
@@ -1,34 +1,37 @@
-function ScajorpRPC(url) {
-
-    this.url = url
-
-    this.xhr = document.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP");
-
-    this.systemList = function() {
-
+if (Function.prototype.bind == null) {
+    Function.prototype.bind = function(object) {
+        var method = this;
+        return function() {
+            return method.apply(object, arguments);
+        };
     }
-
-    //this.onStateChange /
-    var params = '{\"jsonrpc\": \"2.0\", \"method\": \"system.listMethods\", \"params\": [], \"id\": 1}'
-    xhr.open('POST', 'scajorp', true);
-    req.setRequestHeader("Content-Type","application/json-rpc");
-    xhr.setRequestHeader("Content-length", params.length);
-    xhr.onreadystatechange = function() {
-	if(http.readyState == 4 && http.status == 200) {
-		alert(http.responseText);
-	}
-
-    xhr.send(params)
 }
 
-function sendRequest(url,params,HttpMethod){
-  if (!HttpMethod){
-    HttpMethod="POST";
-  }
-  var req=getXMLHTTPRequest();
-  if (req){
-    req.open(HttpMethod,url,true);
+function Scajorp(url) {
+    this.url = url;
+    var xhr = this.xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP");
 
-    req.send(params);
-  }
+    this.post = function(data, callback) {
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json-rpc");
+        xhr.setRequestHeader("Content-length", data.length);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var jsonString = xhr.responseText;
+                callback(eval('(' + jsonString + ')'), jsonString);
+            }
+        };
+        xhr.send(data);
+    };
+
+    this.init = function() {
+        var data = '{\"jsonrpc\": \"2.0\", \"method\": \"system.listMethods\", \"params\": [], \"id\": 1}';
+        this.post(data, poop);
+    }
+    this.init();
+
+}
+
+function poop() {
+    alert('muh');
 }
--- scajorp-examples/src/main/webapp/demos/index_raw.js
+++ scajorp-examples/src/main/webapp/demos/index_raw.js
@@ -1,34 +1,24 @@
-var xhr = window.XMLHttpRequest ? new XMLHttpRequest(): new ActiveXObject("Msxml2.XMLHTTP");
+
 
 function retrieveBlogs() {
-    
-    var params = '{\"jsonrpc\": \"2.0\", \"method\": \"blogEntryService.getAll\", \"params\": [], \"id\": 1}';
-    xhr.open('POST', '/scajorp/', true);
-    xhr.setRequestHeader("Content-Type", "application/json-rpc");
-    xhr.setRequestHeader("Content-length", params.length);
-    xhr.onreadystatechange = parseBlogEntries
-    document.getElementById('jsonRequest').innerHTML = 'Request POST-Body ==> ' + params;
-    xhr.send(params)
+    var data = '{\"jsonrpc\": \"2.0\", \"method\": \"blogEntryService.getAll\", \"params\": [], \"id\": 1}';
+    document.getElementById('jsonRequest').innerHTML = 'Request POST-Body ==> ' + data;
+    var scajorp = new Scajorp("/scajorp/");
+    scajorp.post(data, parseBlogEntries);
 
 }
 
-function parseBlogEntries( ) {
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            var jsonString = xhr.responseText;
-            document.getElementById('jsonResponse').innerHTML = 'Response String ==> ' + jsonString ;
-            var response = eval('(' + jsonString + ')');
-            var blogEntries = response.result;
-            var out = '<table><tr><th>Title</th><th>Author</th></tr>';            
-            for (i = 0; i < blogEntries.length; i++) {                
-                var blog = blogEntries[i];
-                out += '<tr>';
-                out += '<td>'+ blog.title +'</td><td>' + blog.author + '</td>';
-                out += '</tr>';
-            }
-            out += '</table>';
-            document.getElementById('blogentries').innerHTML = out;
-        } else
-            alert('There was a problem retrieving the data: \n' + xhr.statusText);       
+function parseBlogEntries(jsonObj, jsonString) {
+    document.getElementById('jsonResponse').innerHTML = 'Response String ==> ' + jsonString;
+    var blogEntries = jsonObj.result;
+    var out = '<table><tr><th>Title</th><th>Author</th></tr>';
+    for (i = 0; i < blogEntries.length; i++) {
+        var blog = blogEntries[i];
+        out += '<tr>';
+        out += '<td>' + blog.title + '</td><td>' + blog.author + '</td>';
+        out += '</tr>';
     }
+    out += '</table>';
+    document.getElementById('blogentries').innerHTML = out;
+
 }
