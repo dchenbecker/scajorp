6192be12ae2601c804ad73032129fe1bcbeeffb9
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-06-22 11:24:45 +0000

    Scajorp-Api removed Application Factory. Using Servlet companion object instead.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@100 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-api/src/main/scala/org/scajorp/http/ScajorpServlet.scala
+++ scajorp-api/src/main/scala/org/scajorp/http/ScajorpServlet.scala
@@ -4,8 +4,13 @@ import java.io.BufferedReader
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.ServletConfig
+
 import org.scajorp.json.JSONRequest
 import org.scajorp.json.JSONResponse
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -17,14 +22,12 @@ import org.scajorp.json.JSONResponse
 */
 class ScajorpServlet extends HttpServlet {
 
-    /**
-    * The factory used to create acdpplication objects
-    */
-    val applicationFactory: TApplicationFactory = new ContextParamApplicationFactory()
-    
+    val logger = LoggerFactory.getLogger(classOf[ScajorpServlet])
+
+
 
     override def init() {
-           val application = applicationFactory.createApplication(getServletConfig())
+           val application = ScajorpServlet.createApplication(getServletConfig())
            application.developmentInfo()
     }
 
@@ -32,13 +35,42 @@ class ScajorpServlet extends HttpServlet {
         
         val jsonRequest = new JSONRequest(req.getReader())
 
-        val application = applicationFactory.createApplication(getServletConfig())
+        val application = ScajorpServlet.createApplication(getServletConfig())
         
-        val jsonResponse = application.execute(jsonRequest)
+        val jsonResponse = application.execute(jsonRequest)        
 
         jsonResponse.toWriter(resp.getWriter(), application.prettyPrint)
 
     }
 
 
+}
+
+object ScajorpServlet {
+
+    /**
+    * context parameter name that must contain the class name of the application
+    */
+    val application_class_param = "applicationClassName";
+
+    def createApplication(config: ServletConfig): ScajorpApplication = {
+        val applicationClassName = config.getInitParameter(application_class_param);
+        val application = applicationClassName match {
+            case className: String => createApplication(className)
+            case null => error("servlet init param [" + application_class_param + "] is missing.")
+        }
+        application
+    }
+
+    private def createApplication(className: String): ScajorpApplication = {
+        val obj = Class.forName(className).newInstance()
+        val application = if (obj.isInstanceOf[ScajorpApplication]) {
+            obj.asInstanceOf[ScajorpApplication]
+        }
+        else {
+            error("Unable to create Scajorp Application[=" + className +"]")
+        }
+        application
+    }
+    
 }
--- scajorp-examples/src/main/webapp/client/scajorp.js
+++ scajorp-examples/src/main/webapp/client/scajorp.js
@@ -1,137 +1,84 @@
 /*
-JSONstring v 1.01
-copyright 2006 Thomas Frank
-(small sanitizer added to the toObject-method, May 2008)
+Copyright (c) 2002 JSON.org
 
-This EULA grants you the following rights:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-Installation and Use. You may install and use an unlimited number of copies of the SOFTWARE PRODUCT.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-Reproduction and Distribution. You may reproduce and distribute an unlimited number of copies of the SOFTWARE PRODUCT either in whole or in part; each copy should include all copyright and trademark notices, and shall be accompanied by a copy of this EULA. Copies of the SOFTWARE PRODUCT may be distributed as a standalone product or included with your own product.
+The Software shall be used for Good, not Evil.
 
-Commercial Use. You may sell for profit and freely distribute scripts and/or compiled scripts that were created with the SOFTWARE PRODUCT.
-
-Based on Steve Yen's implementation:
-http://trimpath.com/project/wiki/JsonLibrary
-
-Sanitizer regExp:
-Andrea Giammarchi 2007
-
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
-JSONstring={
-	compactOutput:false,
-	includeProtos:false,
-	includeFunctions: false,
-	detectCirculars:true,
-	restoreCirculars:true,
-	make:function(arg,restore) {
-		this.restore=restore;
-		this.mem=[];this.pathMem=[];
-		return this.toJsonStringArray(arg).join('');
-	},
-	toObject:function(x){
-		if(!this.cleaner){
-			try{this.cleaner=new RegExp('^("(\\\\.|[^"\\\\\\n\\r])*?"|[,:{}\\[\\]0-9.\\-+Eaeflnr-u \\n\\r\\t])+?$')}
-			catch(a){this.cleaner=/^(true|false|null|\[.*\]|\{.*\}|".*"|\d+|\d+\.\d+)$/}
-		};
-		if(!this.cleaner.test(x)){return {}};
-		eval("this.myObj="+x);
-		if(!this.restoreCirculars || !alert){return this.myObj};
-		if(this.includeFunctions){
-			var x=this.myObj;
-			for(var i in x){if(typeof x[i]=="string" && !x[i].indexOf("JSONincludedFunc:")){
-				x[i]=x[i].substring(17);
-				eval("x[i]="+x[i])
-			}}
-		};
-		this.restoreCode=[];
-		this.make(this.myObj,true);
-		var r=this.restoreCode.join(";")+";";
-		eval('r=r.replace(/\\W([0-9]{1,})(\\W)/g,"[$1]$2").replace(/\\.\\;/g,";")');
-		eval(r);
-		return this.myObj
-	},
-	toJsonStringArray:function(arg, out) {
-		if(!out){this.path=[]};
-		out = out || [];
-		var u; // undefined
-		switch (typeof arg) {
-		case 'object':
-			this.lastObj=arg;
-			if(this.detectCirculars){
-				var m=this.mem; var n=this.pathMem;
-				for(var i=0;i<m.length;i++){
-					if(arg===m[i]){
-						out.push('"JSONcircRef:'+n[i]+'"');return out
-					}
-				};
-				m.push(arg); n.push(this.path.join("."));
-			};
-			if (arg) {
-				if (arg.constructor == Array) {
-					out.push('[');
-					for (var i = 0; i < arg.length; ++i) {
-						this.path.push(i);
-						if (i > 0)
-							out.push(',\n');
-						this.toJsonStringArray(arg[i], out);
-						this.path.pop();
-					}
-					out.push(']');
-					return out;
-				} else if (typeof arg.toString != 'undefined') {
-					out.push('{');
-					var first = true;
-					for (var i in arg) {
-						if(!this.includeProtos && arg[i]===arg.constructor.prototype[i]){continue};
-						this.path.push(i);
-						var curr = out.length;
-						if (!first)
-							out.push(this.compactOutput?',':',\n');
-						this.toJsonStringArray(i, out);
-						out.push(':');
-						this.toJsonStringArray(arg[i], out);
-						if (out[out.length - 1] == u)
-							out.splice(curr, out.length - curr);
-						else
-							first = false;
-						this.path.pop();
-					}
-					out.push('}');
-					return out;
-				}
-				return out;
-			}
-			out.push('null');
-			return out;
-		case 'unknown':
-		case 'undefined':
-		case 'function':
-			if(!this.includeFunctions){out.push(u);return out};
-			arg="JSONincludedFunc:"+arg;
-			out.push('"');
-			var a=['\n','\\n','\r','\\r','"','\\"'];
-			arg+=""; for(var i=0;i<6;i+=2){arg=arg.split(a[i]).join(a[i+1])};
-			out.push(arg);
-			out.push('"');
-			return out;
-		case 'string':
-			if(this.restore && arg.indexOf("JSONcircRef:")==0){
-				this.restoreCode.push('this.myObj.'+this.path.join(".")+"="+arg.split("JSONcircRef:").join("this.myObj."));
-			};
-			out.push('"');
-			var a=['\n','\\n','\r','\\r','"','\\"'];
-			arg+=""; for(var i=0;i<6;i+=2){arg=arg.split(a[i]).join(a[i+1])};
-			out.push(arg);
-			out.push('"');
-			return out;
-		default:
-			out.push(String(arg));
-			return out;
-		}
-	}
-};
+toJsonString = function(arg) {
+    return toJsonStringArray(arg).join('');
+}
+
+toJsonStringArray = function(arg, out) {
+    out = out || new Array();
+    var u; // undefined
+
+    switch (typeof arg) {
+    case 'object':
+        if (arg) {
+            if (arg.constructor == Array) {
+                out.push('[');
+                for (var i = 0; i < arg.length; ++i) {
+                    if (i > 0)
+                        out.push(',\n');
+                    toJsonStringArray(arg[i], out);
+                }
+                out.push(']');
+                return out;
+            } else if (typeof arg.toString != 'undefined') {
+                out.push('{');
+                var first = true;
+                for (var i in arg) {
+                    var curr = out.length; // Record position to allow undo when arg[i] is undefined.
+                    if (!first)
+                        out.push(',\n');
+                    toJsonStringArray(i, out);
+                    out.push(':');
+                    toJsonStringArray(arg[i], out);
+                    if (out[out.length - 1] == u)
+                        out.splice(curr, out.length - curr);
+                    else
+                        first = false;
+                }
+                out.push('}');
+                return out;
+            }
+            return out;
+        }
+        out.push('null');
+        return out;
+    case 'unknown':
+    case 'undefined':
+    case 'function':
+        out.push(u);
+        return out;
+    case 'string':
+        out.push('"')
+        out.push(arg.replace(/(["\\])/g, '\\$1').replace(/\r/g, '').replace(/\n/g, '\\n'));
+        out.push('"');
+        return out;
+    default:
+        out.push(String(arg));
+        return out;
+    }
+}
 
 
 if (Function.prototype.bind == null) {
@@ -147,7 +94,7 @@ if (Function.prototype.bind == null) {
 function Scajorp(url) {
     this.url = url;
     this.requestId = 1;
-    this.init();  
+    this.init();
 }
 
 
@@ -156,70 +103,65 @@ Scajorp.prototype.init = function() {
     var data = {
         method: 'system.listMethods',
         params: []
-    }          
+    }
     this.post(data, this.addMethods.bind(this));
 }
 
-   
-Scajorp.prototype.post = function(request, callback)  {
 
+Scajorp.prototype.post = function(request, callback) {
 
-        if (typeof request != 'string') {
-            request.id = this.requestId++;
-            request.jsonrpc = "2.0";
-            request = JSONstring.make(request);
+    if (typeof request != 'string') {
+        request.id = this.requestId++;
+        request.jsonrpc = "2.0";
+        request = toJsonString(request);
+    }
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP");
+    xhr.open('POST', this.url, true);
+    xhr.setRequestHeader("Content-Type", "application/json-rpc");
+    xhr.setRequestHeader("Content-length", request.length);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var jsonString = xhr.responseText;
+            if (callback) callback(eval('(' + jsonString + ')'), jsonString);
         }
-
-        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP");
-        xhr.open('POST', this.url, true);
-        xhr.setRequestHeader("Content-Type", "application/json-rpc");
-        xhr.setRequestHeader("Content-length", request.length);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var jsonString = xhr.responseText;
-                callback(eval('(' + jsonString + ')'), jsonString);
-            }
-        };
-        xhr.send(request);
+    };
+    xhr.send(request);
 }
 
 
 Scajorp.prototype.createMethod = function(name) {
-       var fn = function() {            
-            var parameters = [];
+    
+    var fn = function() {
 
-            var args = Array.prototype.slice.call(arguments);  
+        var parameters = Array.prototype.slice.call(arguments);
 
-            var callback = (typeof args[0] == 'function') ? args.shift(): null;
-            for (var i in arguments) {
-                if (typeof arguments[i] !== 'function') parameters.push(args[i]);
-            }
+        var callback = (typeof parameters[0] == 'function') ? parameters.shift() : null;
+        
+        var request = {'method': fn.methodName, 'params': parameters};
+        
+        fn.client.post(request, callback);
 
-           var request = {};           
-           request.method = fn.methodName;
-           request.params = parameters;
-           fn.client.post(request, callback);
-
-        };
-        fn.client = this;
-        fn.methodName = name;
-        return fn;
+    };
+    fn.client = this;
+    fn.methodName = name;
+    return fn;
 
 }
 
 Scajorp.prototype.addMethods = function(jsonResponse) {
-    
-        var methods = jsonResponse.result;
 
-        for (var i in methods) {
-            var name = methods[i].split(".");
-            var objectName = name[0];
-            var methodName = name[1];
+    var methods = jsonResponse.result;
 
-            if (!this[objectName]) this[objectName] = {};
-            
-            this[objectName][methodName] = this.createMethod(objectName + "." + methodName);                     
-        }
+    for (var i in methods) {
+        var name = methods[i].split(".");
+        var objectName = name[0];
+        var methodName = name[1];
+
+        if (!this[objectName]) this[objectName] = {};
+
+        this[objectName][methodName] = this.createMethod(objectName + "." + methodName);
+    }
 
 
 }
