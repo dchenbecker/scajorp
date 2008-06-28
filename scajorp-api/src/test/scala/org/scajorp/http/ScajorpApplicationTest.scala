add5536f71b4056d4a04032a4a822d69e3868a40
behlma <behlma@24bb8ead-1b48-0410-9278-47088e7565d6>
2008-06-28 15:57:10 +0000

    Refactored test class.
    git-svn-id: https://scajorp.googlecode.com/svn/trunk@103 24bb8ead-1b48-0410-9278-47088e7565d6

--- scajorp-api/src/test/scala/org/scajorp/http/ScajorpApplicationTest.scala
+++ scajorp-api/src/test/scala/org/scajorp/http/ScajorpApplicationTest.scala
@@ -11,23 +11,23 @@ import org.junit.Test
 import org.junit.Before
 import org.junit.After
 import org.junit.Assert._
+
 import org.scajorp.json.request.JSONRequest
-import org.scajorp.json.response.ValidResponse
-import java.io.BufferedReader
-import java.io.StringReader
+import org.scajorp.json.response.{ValidResponse,ErrorResponse}
+
+
 
 class ScajorpApplicationTest {
       
     
     val application = new BogusApplication()
 
-    val jsonRequest = new JSONRequest("{\"jsonrpc\": \"2.0\", \"method\": \"calculator.sum\", \"params\": [4, 5], \"id\": 5}")
+    val calculateRequest = new JSONRequest("{\"jsonrpc\": \"2.0\", \"method\": \"calculator.sum\", \"params\": [4, 5], \"id\": 5}")
     
     val systemListRequest = new JSONRequest("{\"jsonrpc\": \"2.0\", \"method\": \"system.listMethods\", \"params\": [], \"id\": 1}")
 
-    val invalidJsonRequest = new JSONRequest("{\"jsonrpc\": \"2.0\", \"method\": \"calculator.sum\", \"params\": [10, 9,6], \"id\": 5}")
+    val invalidJsonRequest = new JSONRequest("{\"jsonrpc\": \"2.0\", \"m}ethod\":.:.. \"calculator.sum\", \"params\": [10, 5], \"id\": 5}")
     
-
        
     @Before
     def setUp() {    
@@ -35,21 +35,25 @@ class ScajorpApplicationTest {
     }
         
     @Test
-    def execute() {
-        val response = application.execute(jsonRequest)
+    def calculate() {
+        val response = application.execute(calculateRequest)
         assertEquals(response, ValidResponse("2.0", 9, 5));
-    }
-        
-    @Test{val expected = classOf[IllegalArgumentException]}
-    def execute_wrongParams() {        
-        application.execute(invalidJsonRequest)         
     }
 
     @Test
     def listMethods() {
-        val response = application.execute(systemListRequest)
-        assertEquals("{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":[\"calculator.sum\"]}", response.toJSON(false))
+        val response = application.execute(systemListRequest)       
+        assertEquals(response, ValidResponse("2.0", List("calculator.sum"), 1));
     }
+
+    /*@Test
+    def invalidJSON() {
+        val response = application.execute(invalidJsonRequest)
+        assertEquals(response, ErrorResponse("2.0", Map("code" -> -32700, "message" -> "Parse Error"), 1));
+    }*/
+        
+  
+    
 
     
 }
